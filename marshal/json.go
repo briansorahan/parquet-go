35423b50d67488ee2511b69351f402d9d2220457
Brian Sorahan <brian.sorahan@drillinginfo.com>
2020-09-17 08:32:54 -0500

    factor out marshalNode helper

--- marshal/json.go
+++ marshal/json.go
@@ -71,184 +71,192 @@ func MarshalJSON(ss []interface{}, schemaHandler *schema.SchemaHandler) (tb *map
 			node = stack[ln-1]
 			stack = stack[:ln-1]
 
-			tk := node.Val.Type().Kind()
-
-			pathStr := node.PathMap.Path
-
-			schemaIndex, ok := schemaHandler.MapIndex[pathStr]
-			//no schema item will be ignored
-			if !ok {
-				continue
+			newStack, err := marshalNode(res, schemaHandler, stack, nodeBuf, node)
+			if err != nil {
+				return nil, err
 			}
-
-			schema := schemaHandler.SchemaElements[schemaIndex]
-
-			if tk == reflect.Map {
-				keys := node.Val.MapKeys()
-
-				if schema.GetConvertedType() == parquet.ConvertedType_MAP { //real map
-					pathStr = pathStr + ".Key_value"
-					if len(keys) <= 0 {
-						for key, table := range res {
-							if strings.HasPrefix(key, node.PathMap.Path) &&
-								(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
-								table.Values = append(table.Values, nil)
-								table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
-								table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
-							}
-						}
-					}
-
-					rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
-					for j := len(keys) - 1; j >= 0; j-- {
-						key := keys[j]
-						value := node.Val.MapIndex(key).Elem()
-
-						newNode := nodeBuf.GetNode()
-						newNode.PathMap = node.PathMap.Children["Key_value"].Children["Key"]
-						newNode.Val = key
-						newNode.DL = node.DL + 1
-						if j == 0 {
-							newNode.RL = node.RL
-						} else {
-							newNode.RL = rlNow
-						}
-						stack = append(stack, newNode)
-
-						newNode = nodeBuf.GetNode()
-						newNode.PathMap = node.PathMap.Children["Key_value"].Children["Value"]
-						newNode.Val = value
-						newNode.DL = node.DL + 1
-						newPathStr := newNode.PathMap.Path // check again
-						newSchemaIndex := schemaHandler.MapIndex[newPathStr]
-						newSchema := schemaHandler.SchemaElements[newSchemaIndex]
-						if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL { //map value only be :optional or required
-							newNode.DL++
-						}
-
-						if j == 0 {
-							newNode.RL = node.RL
-						} else {
-							newNode.RL = rlNow
-						}
-						stack = append(stack, newNode)
-					}
-
-				} else { //struct
-					keysMap := make(map[string]int)
-					for j := 0; j < len(keys); j++ {
-						//ExName to InName
-						keysMap[common.StringToVariableName(keys[j].String())] = j
-					}
-					for key := range node.PathMap.Children {
-						ki, ok := keysMap[key]
-
-						if ok && node.Val.MapIndex(keys[ki]).Elem().IsValid() {
-							newNode := nodeBuf.GetNode()
-							newNode.PathMap = node.PathMap.Children[key]
-							newNode.Val = node.Val.MapIndex(keys[ki]).Elem()
-							newNode.RL = node.RL
-							newNode.DL = node.DL
-							newPathStr := newNode.PathMap.Path
-							newSchemaIndex := schemaHandler.MapIndex[newPathStr]
-							newSchema := schemaHandler.SchemaElements[newSchemaIndex]
-							if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL {
-								newNode.DL++
-							}
-							stack = append(stack, newNode)
-
-						} else {
-							newPathStr := node.PathMap.Children[key].Path
-							for path, table := range res {
-								if strings.HasPrefix(path, newPathStr) &&
-									(len(path) == len(newPathStr) || path[len(newPathStr)] == '.') {
-
-									table.Values = append(table.Values, nil)
-									table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
-									table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
-								}
-							}
-						}
-					}
-				}
-
-			} else if tk == reflect.Slice {
-				ln := node.Val.Len()
-
-				if schema.GetConvertedType() == parquet.ConvertedType_LIST { // real LIST
-					pathStr = pathStr + ".List" + ".Element"
-					if ln <= 0 {
-						for key, table := range res {
-							if strings.HasPrefix(key, node.PathMap.Path) &&
-								(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
-								table.Values = append(table.Values, nil)
-								table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
-								table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
-							}
-						}
-					}
-					rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
-
-					for j := ln - 1; j >= 0; j-- {
-						newNode := nodeBuf.GetNode()
-						newNode.PathMap = node.PathMap.Children["List"].Children["Element"]
-						newNode.Val = node.Val.Index(j).Elem()
-						if j == 0 {
-							newNode.RL = node.RL
-						} else {
-							newNode.RL = rlNow
-						}
-						newNode.DL = node.DL + 1
-
-						newPathStr := newNode.PathMap.Path
-						newSchemaIndex := schemaHandler.MapIndex[newPathStr]
-						newSchema := schemaHandler.SchemaElements[newSchemaIndex]
-						if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL { //element of LIST can only be optional or required
-							newNode.DL++
-						}
-
-						stack = append(stack, newNode)
-					}
-
-				} else { //Repeated
-					if ln <= 0 {
-						for key, table := range res {
-							if strings.HasPrefix(key, node.PathMap.Path) &&
-								(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
-								table.Values = append(table.Values, nil)
-								table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
-								table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
-							}
-						}
-					}
-					rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
-
-					for j := ln - 1; j >= 0; j-- {
-						newNode := nodeBuf.GetNode()
-						newNode.PathMap = node.PathMap
-						newNode.Val = node.Val.Index(j).Elem()
-						if j == 0 {
-							newNode.RL = node.RL
-						} else {
-							newNode.RL = rlNow
-						}
-						newNode.DL = node.DL + 1
-						stack = append(stack, newNode)
-					}
-				}
-
-			} else {
-				table := res[node.PathMap.Path]
-				pT, cT := schema.Type, schema.ConvertedType
-				val := types.JSONTypeToParquetType(node.Val, pT, cT, int(schema.GetTypeLength()), int(schema.GetScale()))
-
-				table.Values = append(table.Values, val)
-				table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
-				table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
-			}
+			stack = newStack
 		}
 	}
 
 	return &res, nil
 
+}
+
+func marshalNode(res map[string]*layout.Table, schemaHandler *schema.SchemaHandler, stack []*Node, nodeBuf *NodeBufType, node *Node) ([]*Node, error) {
+	var (
+		tk              = node.Val.Type().Kind()
+		pathStr         = node.PathMap.Path
+		schemaIndex, ok = schemaHandler.MapIndex[pathStr]
+	)
+	// no schema item will be ignored
+	if !ok {
+		return stack, nil
+	}
+	schema := schemaHandler.SchemaElements[schemaIndex]
+
+	if node.Val.Type().Kind() == reflect.Map {
+		keys := node.Val.MapKeys()
+
+		if schema.GetConvertedType() == parquet.ConvertedType_MAP { //real map
+			pathStr = pathStr + ".Key_value"
+			if len(keys) <= 0 {
+				for key, table := range res {
+					if strings.HasPrefix(key, node.PathMap.Path) &&
+						(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
+						table.Values = append(table.Values, nil)
+						table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
+						table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
+					}
+				}
+			}
+
+			rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
+			for j := len(keys) - 1; j >= 0; j-- {
+				key := keys[j]
+				value := node.Val.MapIndex(key).Elem()
+
+				newNode := nodeBuf.GetNode()
+				newNode.PathMap = node.PathMap.Children["Key_value"].Children["Key"]
+				newNode.Val = key
+				newNode.DL = node.DL + 1
+				if j == 0 {
+					newNode.RL = node.RL
+				} else {
+					newNode.RL = rlNow
+				}
+				stack = append(stack, newNode)
+
+				newNode = nodeBuf.GetNode()
+				newNode.PathMap = node.PathMap.Children["Key_value"].Children["Value"]
+				newNode.Val = value
+				newNode.DL = node.DL + 1
+				newPathStr := newNode.PathMap.Path // check again
+				newSchemaIndex := schemaHandler.MapIndex[newPathStr]
+				newSchema := schemaHandler.SchemaElements[newSchemaIndex]
+				if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL { //map value only be :optional or required
+					newNode.DL++
+				}
+
+				if j == 0 {
+					newNode.RL = node.RL
+				} else {
+					newNode.RL = rlNow
+				}
+				stack = append(stack, newNode)
+			}
+
+		} else { //struct
+			keysMap := make(map[string]int)
+			for j := 0; j < len(keys); j++ {
+				//ExName to InName
+				keysMap[common.StringToVariableName(keys[j].String())] = j
+			}
+			for key := range node.PathMap.Children {
+				ki, ok := keysMap[key]
+
+				if ok && node.Val.MapIndex(keys[ki]).Elem().IsValid() {
+					newNode := nodeBuf.GetNode()
+					newNode.PathMap = node.PathMap.Children[key]
+					newNode.Val = node.Val.MapIndex(keys[ki]).Elem()
+					newNode.RL = node.RL
+					newNode.DL = node.DL
+					newPathStr := newNode.PathMap.Path
+					newSchemaIndex := schemaHandler.MapIndex[newPathStr]
+					newSchema := schemaHandler.SchemaElements[newSchemaIndex]
+					if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL {
+						newNode.DL++
+					}
+					stack = append(stack, newNode)
+
+				} else {
+					newPathStr := node.PathMap.Children[key].Path
+					for path, table := range res {
+						if strings.HasPrefix(path, newPathStr) &&
+							(len(path) == len(newPathStr) || path[len(newPathStr)] == '.') {
+
+							table.Values = append(table.Values, nil)
+							table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
+							table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
+						}
+					}
+				}
+			}
+		}
+
+	} else if tk == reflect.Slice {
+		ln := node.Val.Len()
+
+		if schema.GetConvertedType() == parquet.ConvertedType_LIST { // real LIST
+			pathStr = pathStr + ".List" + ".Element"
+			if ln <= 0 {
+				for key, table := range res {
+					if strings.HasPrefix(key, node.PathMap.Path) &&
+						(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
+						table.Values = append(table.Values, nil)
+						table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
+						table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
+					}
+				}
+			}
+			rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
+
+			for j := ln - 1; j >= 0; j-- {
+				newNode := nodeBuf.GetNode()
+				newNode.PathMap = node.PathMap.Children["List"].Children["Element"]
+				newNode.Val = node.Val.Index(j).Elem()
+				if j == 0 {
+					newNode.RL = node.RL
+				} else {
+					newNode.RL = rlNow
+				}
+				newNode.DL = node.DL + 1
+
+				newPathStr := newNode.PathMap.Path
+				newSchemaIndex := schemaHandler.MapIndex[newPathStr]
+				newSchema := schemaHandler.SchemaElements[newSchemaIndex]
+				if newSchema.GetRepetitionType() == parquet.FieldRepetitionType_OPTIONAL { //element of LIST can only be optional or required
+					newNode.DL++
+				}
+
+				stack = append(stack, newNode)
+			}
+
+		} else { //Repeated
+			if ln <= 0 {
+				for key, table := range res {
+					if strings.HasPrefix(key, node.PathMap.Path) &&
+						(len(key) == len(node.PathMap.Path) || key[len(node.PathMap.Path)] == '.') {
+						table.Values = append(table.Values, nil)
+						table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
+						table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
+					}
+				}
+			}
+			rlNow, _ := schemaHandler.MaxRepetitionLevel(common.StrToPath(pathStr))
+
+			for j := ln - 1; j >= 0; j-- {
+				newNode := nodeBuf.GetNode()
+				newNode.PathMap = node.PathMap
+				newNode.Val = node.Val.Index(j).Elem()
+				if j == 0 {
+					newNode.RL = node.RL
+				} else {
+					newNode.RL = rlNow
+				}
+				newNode.DL = node.DL + 1
+				stack = append(stack, newNode)
+			}
+		}
+
+	} else {
+		table := res[node.PathMap.Path]
+		pT, cT := schema.Type, schema.ConvertedType
+		val := types.JSONTypeToParquetType(node.Val, pT, cT, int(schema.GetTypeLength()), int(schema.GetScale()))
+
+		table.Values = append(table.Values, val)
+		table.DefinitionLevels = append(table.DefinitionLevels, node.DL)
+		table.RepetitionLevels = append(table.RepetitionLevels, node.RL)
+	}
+	return stack, nil
 }
